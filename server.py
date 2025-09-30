8c3c4085974f19956a7e359113e3328dad716d8f
Yomi-09 <alicia345gavidia@gmail.com>
2025-09-30 07:49:47 -0600

    Terminación del CRUD alumnos

--- server.py
+++ server.py
@@ -1,20 +1,27 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-port = 3000
+from urllib import parse
+from urllib.parse import urlparse, parse_qs
+import json 
+import crud_alumno
+
+@@ -9,6 +10,10 @@
+
 class miServidor(SimpleHTTPRequestHandler):
     def do_GET(self):
+        url_parseada = urlparse(self.path)
+        path = url_parseada.path
+        parametros = parse_qs(url_parseada.query)
+
         if self.path=="/":
-            self.path = "/index.html"
+            self.path="index.html"
             return SimpleHTTPRequestHandler.do_GET(self)
+@@ -17,6 +22,9 @@ def do_GET(self):
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(json.dumps(alumnos).encode('utf-8'))
+        if path=="/vistas":
+            self.path = '/modulos/'+ parametros['form'][0] +'.html'
+            return SimpleHTTPRequestHandler.do_GET(self)
+
     def do_POST(self):
         longitud = int(self.headers['Content-Length'])
-        datos = self.rfile.read(longitud)
-        datos = datos.decode("utf-8")
-        datos = parse.unquote(datos)
-        print(datos)
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(datos.encode)("utp")
-                
-print("Servidor ejecutandose")
-server = HTTPServer(("localhost",port),miServidor)
-server.serve_forever()
